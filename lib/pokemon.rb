763ffd80eccdc3945efe3a664aa7a78cf51db92a
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-01-23 22:59:23 -0600

    commit tree 68d82a6888b376de493ae6ac0067aeb29ba6271e

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,32 @@
 class Pokemon
+
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db  #don't understand the concept of the @db
+    end
+
+    def self.save(name, type, db)
+        db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+        # query =<<-TAB
+        # INSERT INTO pokemon 
+        # VALUES (?, ?, ?)
+        # TAB
+        #DB[:conn].execute(query, self.name, self.type)
+        #@id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
+
+    def self.find(id, db)
+        query =<<-SQL 
+        SELECT * FROM pokemon
+        WHERE id = ?
+        SQL
+        res = db.execute(query, id).flatten
+        id, name, type = res
+        self.new(id: id, name: name, type: type, db: db)
+    end
+
 end
